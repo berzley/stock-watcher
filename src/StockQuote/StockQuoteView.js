a96c2d2d1eb83e2d6ed72b73c10d6c42bfe993d0
Andrew Berzley <berzley@gmail.com>
2021-11-19 15:53:15 -0500

    Basic card styling with graph and some responsive styling

--- src/StockQuote/StockQuoteView.js
+++ src/StockQuote/StockQuoteView.js
@@ -18,5 +18,31 @@ export const StockQuoteView = Marionette.View.extend({
      */
     formatMoney: function(value) {
         return Math.round(parseFloat(value)*100)/100
+    },
+
+    /**
+     * Indicate if the value has increased or decreased
+     * @param  {Number} value 
+     * @return {String}
+     */
+    increaseDecrease: function(value) {
+        if(value == 0) {
+            return ''
+        }
+
+        return value > 0 ? 'increase' : 'decrease'
+    },
+
+    /**
+     * Percentage range the value falls in a given range
+     * @param  {Number} value
+     * @param  {Number} low  
+     * @param  {Number} high 
+     * @return {Number}      
+     */
+    currentValuePercent: function(value, low, high) {
+        if(low == high) { return 0; }
+
+        return Math.floor(100 * ( (parseFloat(value)-parseFloat(high)) / (parseFloat(low)-parseFloat(high)) ))
     }
 })
